2609dcf4559d76c43ad3e2bdd9b87371e2554172
Sardorbek9343 <tolibjonovsardorbek12@gmail.com>
2023-12-08 02:22:39 +0500

    BarberShop asset added

---
+++ src/BarbersHub.Api/Controllers/Assets/BarberShopAssetsController.cs
@@ -0,0 +1,55 @@
+﻿using BarbersHub.Api.Models;
+using BarbersHub.Domain.Entities.BarberShops;
+using BarbersHub.Service.Configurations;
+using BarbersHub.Service.Interfaces.Assets;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BarbersHub.Api.Controllers.Assets;
+
+public class BarberShopAssetsController : BaseController
+{
+    private readonly IBarberShopAssetService _assetService;
+
+    public BarberShopAssetsController(IBarberShopAssetService assetService)
+    {
+        _assetService = assetService;
+    }
+    [HttpPost("{barberShop-id}")]
+    public async Task<IActionResult> PostAsync([FromRoute(Name = "barberShop-id")] long barberShopId, [Required] IFormFile formFile)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this._assetService.CreateAsync(barberShopId, formFile)
+        });
+
+    [HttpGet("{barberShop-id}")]
+    public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params, [FromRoute(Name = "barberShop-id")] long barberShopId)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this._assetService.RetrieveAllAsync(barberShopId, @params)
+        });
+
+    [HttpGet("{barberShop-id}/{id}")]
+    public async Task<IActionResult> GetByIdAsync([FromRoute(Name = "barberShop-id")] long userId, [FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this._assetService.RetrieveByIdAsync(userId, id)
+
+        });
+
+    [HttpDelete("{barberShop-id}/{id}")]
+    public async Task<IActionResult> DeleteAsync([FromRoute(Name = "barberShop-id")] long barberShopId, [FromRoute(Name = "id")] long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this._assetService.RemoveAsync(barberShopId, id)
+        });
+
+}
--- src/BarbersHub.Api/Extensions/ServiceExtension.cs
+++ src/BarbersHub.Api/Extensions/ServiceExtension.cs
@@ -1,18 +1,17 @@
-﻿using BarbersHub.Service.Mappers;
+﻿using BarbersHub.Data.IRepositories;
 using BarbersHub.Data.Repositories;
-using BarbersHub.Data.IRepositories;
-using BarbersHub.Service.Interfaces.Assets;
-using BarbersHub.Service.Services.Assets;
 using BarbersHub.Service.Helpers;
-using System.Security.Principal;
-using BarbersHub.Service.Interfaces.Users;
-using BarbersHub.Service.Services.Users;
+using BarbersHub.Service.Interfaces.Assets;
 using BarbersHub.Service.Interfaces.BarberShops;
+using BarbersHub.Service.Interfaces.Users;
+using BarbersHub.Service.Mappers;
+using BarbersHub.Service.Services.Assets;
 using BarbersHub.Service.Services.BarberShops;
+using BarbersHub.Service.Services.Users;
 
 namespace BarbersHub.Api.Extensions;
 
-public static class ServiceExtension 
+public static class ServiceExtension
 {
     public static void AddCustomService(this IServiceCollection services)
     {
@@ -26,5 +25,7 @@ public static class ServiceExtension
         services.AddScoped<IUserAssetService, UserAssetService>();
 
         services.AddScoped<IBarberShopService, BarberShopService>();
+
+        services.AddScoped<IBarberShopAssetService, BarberShopAssetService>();
     }
 }
